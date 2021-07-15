45155e31b34a43fc1360431a4c2f976a8a7d93b0
Aleksey Pravdin <leks16@bk.ru>
2021-07-15 21:19:06 +0300

    проект разбит на компоненты

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,55 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <div class="app">
+        <PostForm
+                @create="createPost"
+        />
+        <PostList
+                :posts="posts"
+        />
+    </div>
 </template>
 
+<script>
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
+
+export default {
+    components: {
+        PostForm,
+        PostList
+    },
+    data() {
+        return {
+            // likes: 0,
+            // dislikes: 0,
+            posts: [
+                {id: 1, title: 'JavaScript', body: 'Описание поста'},
+                {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+                {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
+            ],
+        }
+    },
+    methods: {
+        createPost(post) {
+            this.posts.push(post)
+        },
+
+        // addLike() {
+        //     this.likes += 1
+        // },
+        // addDislike() {
+        //     this.dislikes += 1
+        // }
+    }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
